a014f3775ef6882cb330a6f39f3f804d8c6b6307
mmaihom <60010033@kmitl.ac.th>
2020-09-10 19:34:15 +0700

    vuetify

---
+++ weather/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+      app
+      color="#0A275A"
+      dark
+    >
+      <div class="d-flex align-center">
+          <h1>WEATHER REPORT</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+
+}
+</script>
+
+<style>
+</style>
