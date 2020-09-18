305a4177557d2ea4c7fe4f908a3acb50c22009ec
bowruto <60010980@kmitl.ac.th>
2020-09-18 15:13:16 +0700

    Update GGMap

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-app>
+  <v-app>  
     <MyHeader />
     <Drawer></Drawer>
+    <google-map />
   </v-app>
 </template>
 
 <script>
 import MyHeader from "./components/Header";
 import Drawer from "./components/NavDraw";
+import GoogleMap from "./components/GoogleMap";
+
 
 export default {
   name: "App",
 
   components: {
+    GoogleMap,
     MyHeader,
     Drawer,
   },
---
+++ weather/src/components/GoogleMap.vue
@@ -0,0 +1,66 @@
+<template>
+  <div style="position: fix; height: 100%; width: 100%; padding: 0px; border-width: 0px; margin: 0px; left: 0px; top: 0px;">
+    <GmapMap
+        :center="{lat:10, lng:10}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 100%; height: 100%"
+    >
+        <GmapMarker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="true"
+            @click="center=m.position"
+        />
+    </GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GoogleMap",
+  data() {
+    return {
+      // default to Montreal to keep it simple
+      // change this to whatever makes sense
+      center: { lat: -34.397, lng: 150.644 },
+      markers: [],
+      places: [],
+      currentPlace: null
+    };
+  },
+
+  mounted() {
+    this.geolocate();
+  },
+
+  methods: {
+    // receives a place object via the autocomplete component
+    setPlace(place) {
+      this.currentPlace = place;
+    },
+    // addMarker() {
+    //   if (this.currentPlace) {
+    //     const marker = {
+    //       lat: this.currentPlace.geometry.location.lat(),
+    //       lng: this.currentPlace.geometry.location.lng()
+    //     };
+    //     this.markers.push({ position: marker });
+    //     this.places.push(this.currentPlace);
+    //     this.center = marker;
+    //     this.currentPlace = null;
+    //   }
+    // },
+    geolocate: function() {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+      });
+    }
+  }
+};
+</script>
