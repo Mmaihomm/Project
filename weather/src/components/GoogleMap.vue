92ff3e401d1da0214eb1d7eadf28adcabcb4387d
bowruto <60010980@kmitl.ac.th>
2020-11-11 22:18:43 +0700

    edit show info

--- weather/src/components/GoogleMap.vue
+++ weather/src/components/GoogleMap.vue
@@ -78,19 +78,34 @@
         isSearch: false,
         selected: null,
         station: [
-          {names: 'HS2AR-10', latitude: 15.6454, longitude: 100.2218},
-          {names: 'E24YPM-1', latitude: 16.7646, longitude: 100.0568},
-          {names: 'HS6AB-10', latitude: 15.9948, longitude: 101.07},
-          {names: 'HS3LSE-11', latitude: 14.7201, longitude: 103.5686},
-          {names: 'FW1926', latitude: 13.7177, longitude: 100.5955},
-          {names: 'FW6985', latitude: 13.7472, longitude: 100.6672},
-          {names: 'HS1IFU-13', latitude: 13.8347, longitude: 100.6602},
-          {names: 'E25ECY-1', latitude: 13.9333, longitude: 100.7225},
-          {names: 'HS7AP-10', latitude: 12.9002, longitude: 99.7822},
-          {names: 'HS2AR-10', latitude: 12.7177, longitude: 101.2919},
-          {names: 'E21TMW-3', latitude: 13.0211, longitude: 101.8375},
-          {names: 'HS8AC-10', latitude: 10.9138, longitude: 99.3086},
+          {names: 'HS2AR-10', latitude: 15.6454, longitude: 100.2218, id: 1},
+          {names: 'E24YPM-1', latitude: 16.7646, longitude: 100.0568, id: 2},
+          {names: 'HS6AB-10', latitude: 15.9948, longitude: 101.07, id: 3},
+          {names: 'HS3LSE-11', latitude: 14.7201, longitude: 103.5686, id: 4},
+          {names: 'FW1926', latitude: 13.7177, longitude: 100.5955, id: 5},
+          {names: 'FW6985', latitude: 13.7472, longitude: 100.6672, id: 6},
+          {names: 'HS1IFU-13', latitude: 13.8347, longitude: 100.6602, id: 7},
+          {names: 'E25ECY-1', latitude: 13.9333, longitude: 100.7225, id: 8},
+          {names: 'HS7AP-10', latitude: 12.9002, longitude: 99.7822, id: 9},
+          {names: 'HS2AR-10', latitude: 12.7177, longitude: 101.2919, id: 10},
+          {names: 'E21TMW-3', latitude: 13.0211, longitude: 101.8375, id: 11},
+          {names: 'HS8AC-10', latitude: 10.9138, longitude: 99.3086, id: 12},
         ],
+        weathers: [
+          {name:'HS2AR-10', id: 1, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 2, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 3, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 4, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 5, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 6, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 7, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 8, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 9, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 10, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 11, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+          {name:'HS2AR-10', id: 12, temp: 30, humid: 60, press: 1000, pm1: 3, pm25: 3, pm10: 3},
+        ],
+
       };
     },
 
@@ -176,7 +191,13 @@
 
           google.maps.event.addListener(marker, "mouseover", () => {
             infoWindow.setContent(
-              `<div class="header">${this.station[i].names}</div>`
+              `<div id="stationName" style="font-weight:bold; font-size:16px">${this.station[i].names}</div><br>`+
+              `<div style="position: left;"><div id="temp" style="font-size:14px">Temperature : ${this.weathers[i].temp}  ํC</div>`+
+              `<div id="humid" style="font-size:14px">Humidity : ${this.weathers[i].humid} %</div>`+
+              `<div id="press" style="font-size:14px">Pressure : ${this.weathers[i].press} mbar</div></div>`+
+              `<div style="position: left;"><div id="pm1" style="font-size:14px">PM 1.0 ${this.weathers[i].pm1} ug/m3</div>`+
+              `<div id="pm25" style="font-size:14px">PM 2.5 ${this.weathers[i].pm25} ug/m3</div>`+
+              `<div id="pm10" style="font-size:14px">PM 10 ${this.weathers[i].pm10} ug/m3</div></div>`
               );
             infoWindow.open(map, marker);
           })
@@ -200,5 +221,9 @@
     bottom: 0;
     left: 0;
     background: rgba(77,133,233,0.95);
+  }
+
+  #stationName {
+    font-weight: bold;
   }
 </style>
